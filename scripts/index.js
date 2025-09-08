7a6899d857284736480c58880e1b56af46a16b8d
saadferozee <saadferozee@gmail.com>
2025-09-08 16:52:03 +0600

    card section added, dynamic card added

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 // all API Keys
 const getAllLevels = 'https://openapi.programming-hero.com/api/levels/all';
-const getWordsByLevels = 'https://openapi.programming-hero.com/api/level/5';
-const getAllWords = 'https://openapi.programming-hero.com/api/words/all';
-const getWordsDetail = 'https://openapi.programming-hero.com/api/word/5';
+// const getWordsByLevels = 'https://openapi.programming-hero.com/api/level/5';
+// const getAllWords = 'https://openapi.programming-hero.com/api/words/all';
+// const getWordsDetail = 'https://openapi.programming-hero.com/api/word/5';
 
-const getAllLessonsData = () => {
+const loadAllLessonsData = () => {
     // fetch the API Data
     fetch(getAllLevels)
         .then(data => data.json())
@@ -23,7 +23,7 @@ const displayLessons = (data) => {
         // create a new element
         const newButton = document.createElement('div');
         newButton.innerHTML = `
-        <button 
+        <button id="lesson-button-${levelNo}" onclick="loadWordsData(${levelNo})"
             class="btn btn-outline btn-primary text-[12px]">
                 <i class="fa-solid fa-book-open"></i> Lesson - ${levelNo}
         </button>
@@ -33,4 +33,35 @@ const displayLessons = (data) => {
     });
 }
 
-getAllLessonsData();
+const loadWordsData = (no) => {
+    const API = `https://openapi.programming-hero.com/api/level/${no}`;
+    fetch(API)
+        .then(data => data.json())
+        .then(words => displayWord(words.data));
+}
+
+const displayWord = (words) => {
+    const container = document.getElementById('word-meaning-card-container');
+    container.innerHTML = "";
+
+    words.forEach(wordData => {
+        console.log(wordData);
+        const newCard = document.createElement('div');
+        newCard.innerHTML = `
+            <div class="bg-white rounded-lg p-10 space-y-5 text-center">
+                <h2 class="poppins font-semibold text-2xl">${wordData.word}</h2>
+                <h5 class="poppins font-medium text-sm">Meaning / Pronunciation</h5>
+                <h2 class="siliguri font-medium text-3xl">"${wordData.meaning} / ${wordData.pronunciation}"</h2>
+
+                <div class="mt-9 flex justify-between">
+                    <button class="px-3 py-2 bg-[#1A91FF20] rounded-lg text-sm text-[#374957]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="px-3 py-2 bg-[#1A91FF20] rounded-lg text-sm text-[#374957]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
+        `
+        container.appendChild(newCard);
+
+    });
+}
+
+loadAllLessonsData();
